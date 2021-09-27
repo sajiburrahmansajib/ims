6c3b2ace0e78c18ca9defb0840d7c4a235981488
sajiburrahmansajib <mrahman172152@bscse.uiu.ac.bd>
2021-09-27 20:23:00 +0600

    Main Interface Addad

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="styles/w3.css">
+        <title>Inventory Management System</title>
+    </head>
+    <style> 
+    .bgimg {
+    background-image:url('./image/brown2.jpg');
+    background-size: 100% 600px;
+    background-repeat: no-repeat;
+               }
+
+     </style>
+    <body class="bgimg">
+        <ul class="w3-navbar w3-aqua">
+        <li><a href=>Home</a></li>
+
+        <li><a href="home.php">Add seeling info</a></li>
+       
+       
+      <li class="w3-right w3-teal"><a href="sign_login.html">Sign Up/Log In</a></li>
+        
+    </ul>
+        
+        <div class="w3-container w3-display-topmiddle w3-padding-jumbo w3-margin-top">
+            <div>
+                <h1 class="w3-animate-left w3-xxxlarge w3-text-black w3-center font  "> Inventory Management System </h1>
+                <h5 class="w3-animate-right w3-xlarge w3-center font w3-text-green">We Make inventory Better... </h5>
+            </div>   
+        </div>
+        
+        
+    </body>
+    
+</html>
